0415bf61a551be7aec6886150a80f38f9e04dd89
mimimimum <ii_miiint@hotmail.com>
2017-10-07 14:25:36 +0700

    update

--- config/env/development.js
+++ config/env/development.js
@@ -3,5 +3,11 @@ module.exports = {
        // or use 
        mongoUri: 'mongodb://admin:password@localhost:27017/testDB1',
        debug: true,
-       sessionSecret: 'dev_secret_key'
+       sessionSecret: 'dev_secret_key',
+       google: {
+              clientID: '284129056337-0o3bekpr1stb1r239fodessu0l2totgo.apps.googleusercontent.com',
+              clientSecret: '6UJrLlIgu7dup0N3PIAQ5BSm',
+              callbackURL: 'http://localhost:3000/oauth/google/callback'
+       }
+
 }
--- config/express.js
+++ config/express.js
@@ -5,32 +5,37 @@ import bodyParser from 'body-parser';
 import session from 'express-session';
 import passport from 'passport';
 
-module.exports = function() {
-const app = express();
+module.exports = function () {
+      const app = express();
 
-if (process.env.NODE_ENV === "development") {
-app.use(morgan('dev'));
-}
-else {
-app.use(compression());
-}
+      if (process.env.NODE_ENV === "development") {
+            app.use(morgan('dev'));
+      }
+      else {
+            app.use(compression());
+      }
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-       extended: true
-}));
+      app.use(bodyParser.json());
+      app.use(bodyParser.urlencoded({
+            extended: true
+      }));
 
- var config = require('./config');
-       app.use(session({
-              secret: config.sessionSecret,
-              resave: false,
-              saveUninitializeed: true
-       }));
-       app.use(passport.initialize()); // start passport
-       app.use(passport.session()); // use session via express-session
+      var config = require('./config');
+      app.use(session({
+            secret: config.sessionSecret,
+            resave: false,
+            saveUninitializeed: true
+      }));
+      app.use(passport.initialize()); // start passport
+      app.use(passport.session()); // use session via express-session
 
-require('../app/routes/index.route')(app);
-require('../app/routes/user.route')(app);
+      var path = require("path");
+      app.set('views', path.join(__dirname + '/../app/views/home.html'));
+      app.engine('html', require('ejs').renderFile);
+      app.set('view engine', 'ejs');
 
-return app;
+      require('../app/routes/index.route')(app);
+      require('../app/routes/user.route')(app);
+
+      return app;
 }
